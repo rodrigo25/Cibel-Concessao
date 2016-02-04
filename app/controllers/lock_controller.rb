c5eb5ec590bdcdd52e48357a394c5a7e6702f92b
Flavio Kon <flaviokr@gmail.com>
2016-02-04 16:44:14 +0000

    friendly forwarding

--- app/controllers/lock_controller.rb
+++ app/controllers/lock_controller.rb
@@ -2,13 +2,16 @@ class LockController < ApplicationController
 	include ApplicationHelper
   
   def lockscreen
+    session[:request_url] = params[:request_url] if params[:request_url]
     session[:lock] = true
   end
   
   def unlock
     if current_funcionario.authenticate(params[:senha])
       session.delete(:lock)
-      redirect_to root_url
+      @url = session[:request_url]
+      session.delete(:request_url)
+      redirect_to @url || root_url
     else
       redirect_to lockscreen_path
     end
